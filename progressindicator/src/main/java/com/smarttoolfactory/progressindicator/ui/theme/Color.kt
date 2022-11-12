35ef8ac9d61f770f42c8737a4442e6fc83b67ab2
Smart Tool Factory <smarttoolfactory@icloud.com>
2022-11-12 11:19:21 +0300

    add default colors

---
+++ progressindicator/src/main/java/com/smarttoolfactory/progressindicator/ui/theme/Color.kt
@@ -0,0 +1,6 @@
+package com.smarttoolfactory.progressindicator.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val StaticItemColor = Color(0xff757575)
+val DynamicItemColor = Color(0xffEEEEEE)
